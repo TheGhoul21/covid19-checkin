6c5e1eeb918579cd59e9d5eae00ab037676f1050
Luca Simonetti <little.che90@gmail.com>
2020-03-11 07:57:30 +0100

    Updates

---
+++ precache-manifest.9885438e8a23692ec6349be56365c29b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bb043ac4f81aa86eb0724aa63e09ed4",
+    "url": "/covid19-checkin/index.html"
+  },
+  {
+    "revision": "8473ba219dc96f52ceab",
+    "url": "/covid19-checkin/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3933ab7aec16a44b0a01",
+    "url": "/covid19-checkin/static/js/2.cb2da598.chunk.js"
+  },
+  {
+    "revision": "9d99e1640c0dc4b88f767b3f31a7f235",
+    "url": "/covid19-checkin/static/js/2.cb2da598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8473ba219dc96f52ceab",
+    "url": "/covid19-checkin/static/js/main.25f55551.chunk.js"
+  },
+  {
+    "revision": "213c3adfb73c4a96d98d",
+    "url": "/covid19-checkin/static/js/runtime-main.2f90a51f.js"
+  }
+]);
