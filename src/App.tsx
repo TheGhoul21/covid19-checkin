fc08a5e3bfd1c1664b088cc9b907b20939fff39d
fstabilito <francesco.stabilito@h-farm.com>
2020-03-13 18:02:50 +0100

    UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React, { Ref, ChangeEvent } from 'react';
 import logo from './logo.svg';
-import home from './home.jpg'
+import home from './home.jpg';
+import movie from './movie.png';
+import book from './book.png';
+import fitness from './fitness.png';
+import ministero from './ministero.png';
 import './App.css';
 import Map from './Map';
-import { Feed, Button, Icon, Container, Sidebar, Segment, Menu, Grid, Header, Image, Card, Input, Dimmer, Form, Label, List, Modal, Divider } from 'semantic-ui-react'
+import { Feed, Button, Icon, Container, Sidebar, Segment, Menu, Grid, Header, Image, Card, Input, Dimmer, Form, Label, List, Modal, Divider, CardDescription } from 'semantic-ui-react'
 import { geolocated, GeolocatedProps } from "react-geolocated";
 import CheckinDimmer from './CheckinDimmer';
 const comuni = require('./comuni.json');
@@ -72,8 +76,7 @@ function App(props: {} & GeolocatedProps) {
       >
         <Grid style={{ height: '150vh', marginTop: '30vh' }}>
           <Grid.Row style={{}}>
-            <Grid
-              textAlign='center' style={{ height: '100vh', backgroundColor: 'white' }}
+            <Grid textAlign='center' style={{ height: '100vh', backgroundColor: 'white' }}
             >
 
               <Grid.Row columns={1}>
@@ -116,8 +119,8 @@ function App(props: {} & GeolocatedProps) {
       <Sidebar.Pusher>
         <Container style={{ width: '100vw', height: '100vh', overflow: 'none' }} >
         <Divider horizontal />
-          <Header textAlign='center' color='green'>Stiamo a casa, insieme!</Header>
-          <Label textAlign='center' color='green' pointing> <Icon name='home' /> Veronika è a casa</Label>
+          <Header textAlign='center' color='green'>Stiamo a casa, insieme! <p> </p>
+          <Label textAlign='center' color='green' pointing> <Icon name='home' /> Veronika è a casa</Label></Header>
           <Divider horizontal />
           <Dimmer.Dimmable blurring={true} dimmed={active}>
             <CheckinDimmer setActive={setActive} active={active} data={data} zipCode={zipCode} />
@@ -152,7 +155,6 @@ function App(props: {} & GeolocatedProps) {
 
                   }}
                 />
-
                 <Modal open={modalOpen} onClose={() => setModalOpen(false)} basic>
                   <Modal.Content>
                     <h3>Errore: Nessun CAP trovato</h3>
@@ -169,9 +171,56 @@ function App(props: {} & GeolocatedProps) {
           </Dimmer.Dimmable>
           <Button fluid onClick={() => setVisible(true)}><Icon name='arrow up' /> Clicca e guarda in quanti siamo!</Button>
         </Container>
+        <Segment>
+        <Divider horizontal />
+        <Grid.Row textAlign="center">
+        <Header textAlign='center' color='green'>Cosa si può fare? </Header>
+        </Grid.Row>
+        <Divider horizontal />
+        <Grid>
+        <Grid.Row columns={4}>
+>
+                  <Grid.Column>
+                  <Card>
+                      <Card.Header textAlign='center'>Esercizi Caslinghi</Card.Header>
+                      <Image src={fitness} wrapped ui={false} />
+                      <Card.Content textAlign='center'> 
+                        <Button color='green'> <a href ="https://www.google.it/search?sxsrf=ALeKk00EmlJXKgQ5ZjTkfGFWODnF1Lnu-A%3A1584116257561&ei=IbJrXuzuIYuC1fAPjemkiAY&q=esercizi+da+fare+a+casa&oq=esercizi+da+fare&gs_l=psy-ab.3.0.0i131l4j0j0i131l2j0l3.671448.672907..675570...0.5..0.134.1533.12j4......0....1..gws-wiz.......0i71j35i39j0i131i67j0i67.BqhXKNPyJIM" target="_blank">Fatti ispirare!</a> </Button>
+                        </Card.Content>
+                   </Card>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Card>
+                      <Card.Header textAlign='center'>Leggi!</Card.Header>
+                      <Image src={book} wrapped ui={false} />
+                      <Card.Content textAlign='center'> 
+                        <Button color='green'> <a href ="https://www.google.com/search?q=libri+da+leggere&oq=libri+da+&aqs=chrome.0.69i59j0j69i57j0l5.1432j0j4&sourceid=chrome&ie=UTF-8" target="_blank">Cercane qualcuna!</a> </Button>
+                        </Card.Content>
+                    </Card>
+                    </Grid.Column>
+                    <Grid.Column>
+                    <Card>
+                      <Card.Header textAlign='center'>Serie TV</Card.Header>
+                      <Image src={movie} wrapped ui={false} />
+                      <Card.Content textAlign='center'> 
+                        <Button color='green'> <a href ="https://www.google.it/search?sxsrf=ALeKk01qWfRDptACJxM_RnQWLmr6CVwHMQ%3A1584116248385&source=hp&ei=GLJrXvWCFYqtrgSy5yQ&q=Serie+TV+da+vedere&oq=Serie+TV+da+vedere&gs_l=psy-ab.3..0i131l5j0i3j0j0i131l2j0.3743.7820..8052...6.0..0.135.1823.19j2......0....1..gws-wiz.....10..35i362i39j35i39.TOE4sBTFtq8&ved=0ahUKEwj17o_y7JfoAhWKlosKHbIzCQAQ4dUDCAg&uact=5" target="_blank">Cercane qualcuna!</a> </Button>
+                        </Card.Content>
+                    </Card>
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Card>
+                      <Card.Header textAlign='center'>Tieniti informato</Card.Header>
+                      <Image src={ministero} wrapped ui={false} />
+                      <Card.Content textAlign='center'> 
+                        <Button color='green'> <a href ="http://www.salute.gov.it/nuovocoronavirus" target="_blank">Leggi le novità!</a> </Button>
+                        </Card.Content>
+                    </Card>
+                  </Grid.Column>
+          </Grid.Row>
+                </Grid>
+          </Segment>
       </Sidebar.Pusher>
     </Sidebar.Pushable>
-
   );
 }
 
