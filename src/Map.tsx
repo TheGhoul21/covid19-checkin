e6e781b9ef32561e23f73151a35ceced48573af9
Luca <little.che90@gmail.com>
2020-03-13 07:08:40 +0100

    green markers

--- src/Map.tsx
+++ src/Map.tsx
@@ -45,15 +45,15 @@ export const heatmapLayer: LayerProps = {
             0,
             'rgba(33,102,172,0)',
             0.2,
-            'rgb(103,169,207)',
+            'rgb(24,178,43)',
             0.4,
-            'rgb(209,229,240)',
+            'rgb(24,178,43)',
             0.6,
-            'rgb(253,219,199)',
+            'rgb(24,178,43)',
             0.8,
-            'rgb(239,138,98)',
+            'rgb(24,178,43)',
             1,
-            'rgb(178,24,43)'
+            'rgb(24,178,43)'
         ],
         // Adjust the heatmap radius by zoom level
         'heatmap-radius': [
