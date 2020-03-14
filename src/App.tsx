234434ec4f31cf9940ebe143a7fdfe7156297a5d
Luca <little.che90@gmail.com>
2020-03-14 11:37:19 +0100

    remove unsused var

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import movie from './movie.png';
 import book from './book.png';
 import food from './food.png';
 import love from './love.png';
-import pin from './pin.png';
 import fitness from './fitness.png';
 import './App.css';
 import Map from './Map';
@@ -213,7 +212,7 @@ function App(props: {} & GeolocatedProps) {
                   <Card.Header>Un pò di allenamento</Card.Header>
                   <Card.Meta>Ci si allena anche in casa</Card.Meta>
                   <Card.Description>
-                  Hai pensato che potresti tornare in forma <strong>chiuso in casa?</strong>
+                    Hai pensato che potresti tornare in forma <strong>chiuso in casa?</strong>
                   </Card.Description>
                 </Card.Content>
                 <Card.Content extra>
@@ -232,7 +231,7 @@ function App(props: {} & GeolocatedProps) {
                   <Card.Header>Qualche libro?</Card.Header>
                   <Card.Meta>Nutriamo la mente</Card.Meta>
                   <Card.Description>
-                  Cosa c'è di meglio di leggere sul <strong>divano?</strong>
+                    Cosa c'è di meglio di leggere sul <strong>divano?</strong>
                   </Card.Description>
                 </Card.Content>
                 <Card.Content extra>
@@ -295,7 +294,7 @@ function App(props: {} & GeolocatedProps) {
               <Divider horizontal />
               <Header as='h2' color="green">Ringraziamenti</Header>
               <p>
-              L’idea è nata al mattino, intorno alle 06:50; alle 07:15 <a href=" https://www.linkedin.com/in/luca-simonetti/">Luca S.</a> <strong>(GRAZIE!)</strong> aveva già risposto “Facciamolo” e ha iniziato a programmarlo, poi <a href="https://www.linkedin.com/in/caterina-marzolla-b5a575a3">Caterina</a> ci ha aiutato a immaginarlo graficamente e con <a href="https://www.instagram.com/andreaferraroyo/">Andrea</a>  abbiamo messo insieme quello che mancava, grazie anche <a href="https://www.linkedin.com/in/lucalorenzinivittorio/">Luca L.</a> per il supporto.</p>
+                L’idea è nata al mattino, intorno alle 06:50; alle 07:15 <a href=" https://www.linkedin.com/in/luca-simonetti/">Luca S.</a> <strong>(GRAZIE!)</strong> aveva già risposto “Facciamolo” e ha iniziato a programmarlo, poi <a href="https://www.linkedin.com/in/caterina-marzolla-b5a575a3">Caterina</a> ci ha aiutato a immaginarlo graficamente e con <a href="https://www.instagram.com/andreaferraroyo/">Andrea</a>  abbiamo messo insieme quello che mancava, grazie anche <a href="https://www.linkedin.com/in/lucalorenzinivittorio/">Luca L.</a> per il supporto.</p>
               <p> Speriamo possa essere utile in qualche modo, </p>
               <Header as='h2' color="green"><a href=" https://www.linkedin.com/in/frastab/">Francesco</a></Header>
             </Container>
